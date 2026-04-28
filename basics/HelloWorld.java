7c8685324ef18469b5a05c2ce32ae179106a59f9
Rahul Yadav <rahulyadav.prof@gmail.com>
2026-04-28 21:10:16 +0530

    Update greeting message in HelloWorld.java

--- basics/HelloWorld.java
+++ basics/HelloWorld.java
@@ -1,6 +1,6 @@
 public class HelloWorld {
 
     public static void main(String[] args) {
-        System.out.println("Hello, Rahul");
+        System.out.println("Hello, Rahul, 22, BCA");
     }
 }
