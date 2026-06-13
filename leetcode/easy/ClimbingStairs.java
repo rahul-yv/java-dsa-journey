b7690bd80c8528bcc03179d5d6b6f0f0d5720d27
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-14 02:47:52 +0530

    add Climbing Stairs pointer

---
+++ leetcode/easy/ClimbingStairs.java
@@ -0,0 +1,8 @@
+// Climbing Stairs (LeetCode #70 - Easy)
+// Already solved in the dp folder since it's the intro DP problem.
+// See: ../../dp/ClimbingStairs.java
+public class ClimbingStairs {
+    public static void main(String[] args) {
+        System.out.println("see dp/ClimbingStairs.java");
+    }
+}
