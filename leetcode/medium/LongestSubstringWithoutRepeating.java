e4fb1ef5ec0a7b256616b692d72d0b8d6cee9cfc
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-14 02:50:52 +0530

    add Longest Substring pointer

---
+++ leetcode/medium/LongestSubstringWithoutRepeating.java
@@ -0,0 +1,8 @@
+// Longest Substring Without Repeating Characters (LeetCode #3 - Medium)
+// Already solved in the strings folder with the sliding window.
+// See: ../../strings/LongestSubstringWithoutRepeatingCharacters.java
+public class LongestSubstringWithoutRepeating {
+    public static void main(String[] args) {
+        System.out.println("see strings/LongestSubstringWithoutRepeatingCharacters.java");
+    }
+}
