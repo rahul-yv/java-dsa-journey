2910837a9f5377bce6e409f2a522314a5835d554
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-14 02:10:44 +0530

    add Balanced Parentheses pointer

---
+++ stack/BalancedParentheses.java
@@ -0,0 +1,14 @@
+/*
+ * Balanced Parentheses is the same problem as Valid Parentheses, which I already
+ * solved in this folder. Not going to keep two copies.
+ *
+ * See: ValidParenthesesUsingStack.java   (LeetCode #20 - Easy)
+ *
+ * Quick idea: push opening brackets, and when you hit a closing one it must match
+ * whatever's on top of the stack. string is balanced if the stack ends empty.
+ */
+public class BalancedParentheses {
+    public static void main(String[] args) {
+        System.out.println("see ValidParenthesesUsingStack.java in this same folder");
+    }
+}
