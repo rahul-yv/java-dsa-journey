b87f21b24004d7d03132760ea83ac1bb9bcb2c82
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-14 02:48:18 +0530

    add Queue using Stacks pointer

---
+++ leetcode/easy/ImplementQueueUsingStacks.java
@@ -0,0 +1,9 @@
+// Implement Queue using Stacks (LeetCode #232 - Easy)
+// I have the mirror of this (stack using two queues / queue using two stacks)
+// already in the queue folder.
+// See: ../../queue/QueueUsingTwoStacks.java
+public class ImplementQueueUsingStacks {
+    public static void main(String[] args) {
+        System.out.println("see queue/QueueUsingTwoStacks.java");
+    }
+}
