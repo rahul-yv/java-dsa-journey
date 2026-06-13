4c798ad90649c431218e122b7eb44b17d4ad5d8a
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-14 02:15:44 +0530

    add Detect Cycle pointer

---
+++ linked-list/DetectCycleInLinkedList.java
@@ -0,0 +1,14 @@
+/*
+ * Detect Cycle in a Linked List - I already solved this with Floyd's tortoise
+ * and hare in DetectCycle.java, so I'm not duplicating the full thing.
+ *
+ * See: DetectCycle.java   (LeetCode #141 - Easy)
+ *
+ * Quick idea: slow pointer moves 1 step, fast moves 2. if there's a loop they
+ * eventually meet inside it; if fast hits null, no loop.
+ */
+public class DetectCycleInLinkedList {
+    public static void main(String[] args) {
+        System.out.println("see DetectCycle.java in this same folder (Floyd's algorithm)");
+    }
+}
