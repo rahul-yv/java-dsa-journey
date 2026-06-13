167481a5d32ee369090512cbdfe3d55727eff657
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-14 02:48:10 +0530

    add Missing Number pointer

---
+++ leetcode/easy/MissingNumber.java
@@ -0,0 +1,8 @@
+// Missing Number (LeetCode #268 - Easy)
+// Solved with the XOR trick in the bit manipulation folder.
+// See: ../../bitmanipulation/MissingNumber.java
+public class MissingNumber {
+    public static void main(String[] args) {
+        System.out.println("see bitmanipulation/MissingNumber.java");
+    }
+}
