0e2afe84821e2b9093da9f4f699bf4263cdc85e1
Rahul Yadav <rahulyadav.prof@gmail.com>
2026-06-14 02:48:06 +0530

    add Max Depth pointer (#144)

---
+++ leetcode/easy/MaxDepthOfBinaryTree.java
@@ -0,0 +1,8 @@
+// Maximum Depth of Binary Tree (LeetCode #104 - Easy)
+// Same thing as tree height, already done.
+// See: ../../trees/HeightOfBinaryTree.java
+public class MaxDepthOfBinaryTree {
+    public static void main(String[] args) {
+        System.out.println("see trees/HeightOfBinaryTree.java");
+    }
+}
