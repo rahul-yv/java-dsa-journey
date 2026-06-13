509eab59258a772e91212522ac47f761f7959777
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-14 02:51:34 +0530

    add Course Schedule pointer

---
+++ leetcode/medium/CourseSchedule.java
@@ -0,0 +1,8 @@
+// Course Schedule (LeetCode #207 - Medium)
+// Solved with topological sort in the graphs folder.
+// See: ../../graphs/CourseSchedule.java
+public class CourseSchedule {
+    public static void main(String[] args) {
+        System.out.println("see graphs/CourseSchedule.java");
+    }
+}
