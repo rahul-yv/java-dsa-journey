bec6a182eef43453e82af81860a0dace7a8abf18
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-14 02:51:08 +0530

    add Min Stack pointer

---
+++ leetcode/medium/MinStack.java
@@ -0,0 +1,8 @@
+// Min Stack (LeetCode #155 - Medium)
+// Already solved in the stack folder.
+// See: ../../stack/MinStackDemo.java
+public class MinStack {
+    public static void main(String[] args) {
+        System.out.println("see stack/MinStackDemo.java");
+    }
+}
