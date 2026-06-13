7a6efaed0b248111e4429d9365034728aabd06ba
Rahul Yadav <rahulyadav.prof@gmail.com>
2026-06-14 02:51:22 +0530

    add Find Peak Element pointer (#155)

---
+++ leetcode/medium/FindPeakElement.java
@@ -0,0 +1,8 @@
+// Find Peak Element (LeetCode #162 - Medium)
+// Solved with binary search in the binary-search folder.
+// See: ../../binary-search/PeakElement.java
+public class FindPeakElement {
+    public static void main(String[] args) {
+        System.out.println("see binary-search/PeakElement.java");
+    }
+}
