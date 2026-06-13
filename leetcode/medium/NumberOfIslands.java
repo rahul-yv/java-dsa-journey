2bf40d0c22f73af79aacc74a9d2379e8f0b7932f
Rahul Yadav <rahulyadav.prof@gmail.com>
2026-06-14 02:51:30 +0530

    add Number of Islands pointer (#156)

---
+++ leetcode/medium/NumberOfIslands.java
@@ -0,0 +1,8 @@
+// Number of Islands (LeetCode #200 - Medium)
+// Solved with flood-fill DFS in the graphs folder.
+// See: ../../graphs/NumberOfIslands.java
+public class NumberOfIslands {
+    public static void main(String[] args) {
+        System.out.println("see graphs/NumberOfIslands.java");
+    }
+}
