05296b8954248e0d96a4291a4e6c521cf4ad35c9
Rahul Yadav <rahulyadav.prof@gmail.com>
2026-06-15 13:02:16 +0530

    add Trapping Rain Water pointer (#159)

---
+++ leetcode/hard/TrappingRainWater.java
@@ -0,0 +1,8 @@
+// Trapping Rain Water (LeetCode #42 - Hard)
+// Solved with two pointers in the arrays folder.
+// See: ../../arrays/TrappingRainWater.java
+public class TrappingRainWater {
+    public static void main(String[] args) {
+        System.out.println("see arrays/TrappingRainWater.java");
+    }
+}
