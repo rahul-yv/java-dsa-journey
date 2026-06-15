47e57c03e67aa6086ca9731e09e0d44a2b4aebb0
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-15 13:02:50 +0530

    add Word Search II pointer

---
+++ leetcode/hard/WordSearchII.java
@@ -0,0 +1,8 @@
+// Word Search II (LeetCode #212 - Hard)
+// Solved with a trie + DFS in the trie folder.
+// See: ../../trie/WordSearchII.java
+public class WordSearchII {
+    public static void main(String[] args) {
+        System.out.println("see trie/WordSearchII.java");
+    }
+}
