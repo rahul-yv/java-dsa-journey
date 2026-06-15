ff6ab9d16614553b5baa88712cbdc6aac32dcb8d
Rahul Yadav <rahulyadav.prof@gmail.com>
2026-06-15 13:02:35 +0530

    add Sliding Window Maximum pointer (#161)

---
+++ leetcode/hard/SlidingWindowMaximum.java
@@ -0,0 +1,8 @@
+// Sliding Window Maximum (LeetCode #239 - Hard)
+// Solved with a monotonic deque in the queue folder.
+// See: ../../queue/SlidingWindowMaximumUsingDeque.java
+public class SlidingWindowMaximum {
+    public static void main(String[] args) {
+        System.out.println("see queue/SlidingWindowMaximumUsingDeque.java");
+    }
+}
