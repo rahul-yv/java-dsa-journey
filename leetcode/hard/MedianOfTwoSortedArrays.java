0f628b6759b87271494c5bd44d92e71f4c92e5f1
rahulyadavhub <rahulyadav.prof@gmail.com>
2026-06-15 13:03:09 +0530

    add Median of Two Sorted Arrays pointer

---
+++ leetcode/hard/MedianOfTwoSortedArrays.java
@@ -0,0 +1,8 @@
+// Median of Two Sorted Arrays (LeetCode #4 - Hard)
+// Solved with binary search on the cut in the binary-search folder.
+// See: ../../binary-search/MedianOfTwoSortedArrays.java
+public class MedianOfTwoSortedArrays {
+    public static void main(String[] args) {
+        System.out.println("see binary-search/MedianOfTwoSortedArrays.java");
+    }
+}
