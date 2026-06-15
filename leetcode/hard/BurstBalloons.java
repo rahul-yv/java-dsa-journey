11728a0ead044f4e7084e438c7e560ab4fa56b36
Rahul Yadav <rahulyadav.prof@gmail.com>
2026-06-15 13:03:05 +0530

    add Burst Balloons pointer (#164)

---
+++ leetcode/hard/BurstBalloons.java
@@ -0,0 +1,8 @@
+// Burst Balloons (LeetCode #312 - Hard)
+// Solved with interval DP (think "which balloon bursts last") in the dp folder.
+// See: ../../dp/BurstBalloons.java
+public class BurstBalloons {
+    public static void main(String[] args) {
+        System.out.println("see dp/BurstBalloons.java");
+    }
+}
